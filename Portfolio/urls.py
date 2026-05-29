ba96fa933f0974e19b66a429a454ca44fa814715
Vanesa Manzanelli <vanemanza1@gmail.com>
2026-05-28 21:28:22 -0300

    fix path media produccion

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('portfolio.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
